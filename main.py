60a5eec50bc363d9ec86b734473003079cdcf762
Bibek Sah <bkumarsah627@rku.ac.in>
2022-05-01 21:55:42 +0530

    Page title, page icon with and upload format

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import imp
-from re import X
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import numpy as np
+
+PAGE_CONFIG = {"page_title":"Data Visualization","page_icon":"chart_with_upwards_trend:", "layout":"centered"}
+st.set_page_config(**PAGE_CONFIG)
+
 
 def showGraphList():
     """This function will return all the graph available"""    
@@ -21,7 +23,8 @@ def sidebar():
     allowedExtension =['csv', 'xlsx']
     # linegraph = ""
     with st.sidebar:
-        uploaded_file = st.file_uploader("Choose a file")
+        uploaded_file = st.sidebar.file_uploader(label="Upload your csv or excel file (200 MB Max).", type=['csv','xlsx'])
+        # uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
             filename = uploaded_file.name   
             extension = filename[filename.index(".")+1:]
@@ -38,7 +41,7 @@ def sidebar():
                 st.write("File Formate is not supported")
 
 def mainContent():
-    st.header("Data is Tabulaur Formate")
+    st.header("Visualize Your Data")
     if df is not None:
         st.write(df)
         # graph = ["Line Chart", "Bar Chart"]
